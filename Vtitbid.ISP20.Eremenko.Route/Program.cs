371dae7dd283a7faa49332f68bfac736f1717c09
Tepbezdm <keremenko67@gmail.com>
2022-11-16 19:50:14 +0300

    First commit

---
+++ Vtitbid.ISP20.Eremenko.Route/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vtitbid.ISP20.Eremenko.Route
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Route[] route = new Route[3];
+
+            for (int i = 0; i < route.Length; i++)
+            {
+                Console.WriteLine("Начальный пункт маршрута: ");
+                string from = Console.ReadLine();
+                Console.WriteLine("Конечный пункт маршрута: ");
+                string to = Console.ReadLine();
+                Console.WriteLine("Номер маршрута: ");
+                string number = Console.ReadLine();
+                route[i] = new Route(from, to, number);
+            }
+            Route[] routeSort = route.OrderBy(obj => obj.Number).ToArray();
+            for (int i = 0; i < route.Length; i++)
+            {
+                Console.WriteLine(routeSort[i]);
+            }
+            Console.WriteLine("Поиск по номеру маршруту: ");
+            string ser = Console.ReadLine();
+
+            for (int i = 0; i < route.Length; i++)
+            {
+                if (ser.ToLower() == routeSort[i].Number.ToLower())
+                {
+                    Console.WriteLine(routeSort[i]);
+                    break;
+                }
+                if (i == routeSort.Length - 1 && ser.ToLower() != routeSort[i].Number.ToLower())
+                {
+                    Console.WriteLine("Такого маршрута нет");
+                    break;
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Vtitbid.ISP20.Eremenko.Route/Route.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vtitbid.ISP20.Eremenko.Route
+{
+    internal class Route
+    {
+        public string From;
+        public string To;
+        public string Number;
+
+        public Route(string from, string to, string number)
+        {
+            From = from;
+            To = to;
+            Number = number;
+        }
+
+        public override string ToString()
+        {
+            return $"Начальный маршрут: {From}, Конечный маршрут: {To}, Номер маршрута: {Number}";
+        }
+    }
+}
